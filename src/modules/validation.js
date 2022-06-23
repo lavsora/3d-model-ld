30c0d4b2e59076feba11a88625d3b9d2c75c5fde
lavsora <ya.karpich@gmail.com>
2022-06-23 21:20:34 +0300

    validation blur

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -8,22 +8,64 @@ const validation = () => {
     const calcDayInput = document.querySelector('.calc-day');
 
     formNameInput.forEach(inputName => {
-        inputName.addEventListener('input', (e) => e.target.value = e.target.value.replace(/[^а-я\s\-]/gi, ''));
+        inputName.addEventListener('blur', (e) => {
+            let strArr = e.target.value.split(' ');
+
+            for (var i = 0; i < strArr.length; i++) {
+                strArr[i] = strArr[i].charAt(0).toUpperCase() + strArr[i].slice(1);
+            }
+
+            e.target.value = strArr.join(' ');
+
+            e.target.value = e.target.value.replace(/[^а-я\s\-]/gi, '').trim()
+            e.target.value = e.target.value.replace(/\s+/g, ' ');
+            e.target.value = e.target.value.replace(/\--+/g, '-');
+            e.target.value = e.target.value.replace(/(^\-+|\-+$)/g, '');
+        });
     });
 
     formEmailInput.forEach(inputEmail => {
-        inputEmail.addEventListener('input', (e) => e.target.value = e.target.value.replace(/[^\w\@\_\.\!\~\*\'\-]/gi, ''));
+        inputEmail.addEventListener('blur', (e) => {
+            e.target.value = e.target.value.replace(/[^\w\@\_\.\!\~\*\'\-]/gi, '').trim()
+            e.target.value = e.target.value.replace(/\--+/g, '-');
+            e.target.value = e.target.value.replace(/(^\-+|\-+$)/g, '');
+        });
     });
 
     formPhoneInput.forEach(inputPhone => {
-        inputPhone.addEventListener('input', (e) => e.target.value = e.target.value.replace(/[^\(\)\-\d]/g, ''));
+        inputPhone.addEventListener('blur', (e) => {
+            e.target.value = e.target.value.replace(/[^\(\)\-\d]/g, '').trim()
+            e.target.value = e.target.value.replace(/\--+/g, '-');
+            e.target.value = e.target.value.replace(/(^\-+|\-+$)/g, '');
+        });
     });
 
-    formMessInput.addEventListener('input', (e) => e.target.value = e.target.value.replace(/[^а-я\s\-]/gi, ''));
+    formMessInput.addEventListener('blur', (e) => { 
+        let strArr = e.target.value.split(' ');
 
-    calcSquareInput.addEventListener('input', (e) => e.target.value = e.target.value.replace(/\D+/, ''));
-    calcCountInput.addEventListener('input', (e) => e.target.value = e.target.value.replace(/\D+/, ''));
-    calcDayInput.addEventListener('input', (e) => e.target.value = e.target.value.replace(/\D+/, ''));
+            for (var i = 0; i < strArr.length; i++) {
+                strArr[i] = strArr[i].charAt(0).toUpperCase() + strArr[i].slice(1);
+            }
+
+            e.target.value = strArr.join(' ');
+
+            e.target.value = e.target.value.replace(/[^а-я\s\-]/gi, '').trim()
+            e.target.value = e.target.value.replace(/\s+/g, ' ');
+            e.target.value = e.target.value.replace(/\--+/g, '-');
+            e.target.value = e.target.value.replace(/(^\-+|\-+$)/g, '');
+    });
+
+    calcSquareInput.addEventListener('blur', (e) => {
+        e.target.value = e.target.value.replace(/\D+/, '').trim()
+    });
+
+    calcCountInput.addEventListener('blur', (e) => {
+        e.target.value = e.target.value.replace(/\D+/, '').trim()
+    });
+
+    calcDayInput.addEventListener('blur', (e) => {
+        e.target.value = e.target.value.replace(/\D+/, '').trim()
+    });
 }
 
 export default validation;
