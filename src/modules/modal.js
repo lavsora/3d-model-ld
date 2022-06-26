2a8e72bf0ab0c32d32508d8b3d48895b81769175
lavsora <ya.karpich@gmail.com>
2022-06-26 20:06:25 +0300

    animation module

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,37 +1,10 @@
+import { animate, quad, makeEaseOut } from "./animation";
+
 const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
     const popupContent = modal.querySelector('.popup-content');
-
-    const animate = ({ timing, draw, duration }) => {
-        let start = performance.now();
-
-        requestAnimationFrame(function animate(time) {
-            let timeFraction = (time - start) / duration;
-
-            if (timeFraction > 1) timeFraction = 1;
-
-            const progress = timing(timeFraction);
-
-            draw(progress);
-
-            if (timeFraction < 1) requestAnimationFrame(animate);
-        });
-    }
-
-    const quad = (timeFraction) => {
-        return Math.pow(timeFraction, 2)
-    }
-
-    const makeEaseOut = (timing) => {
-        return function (timeFraction) {
-            if (timeFraction < .5)
-                return timing(2 * timeFraction) / 2;
-            else
-                return (2 - timing(2 * (1 - timeFraction))) / 2;
-        }
-    }
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
