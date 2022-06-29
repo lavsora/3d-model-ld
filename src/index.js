0d9c22994fd37955dc2d0f11aea0a607ce51310b
lavsora <ya.karpich@gmail.com>
2022-06-29 16:39:10 +0300

    calc add

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import scroll from './modules/scroll';
-import validation from './modules/validation'
-import tabs from './modules/tabs'
+import validation from './modules/validation';
+import tabs from './modules/tabs';
+import calc from './modules/calc'
 
 timer('23 jule 2022');
 menu();
@@ -11,3 +12,4 @@ modal();
 scroll();
 validation();
 tabs();
+calc(100);
---
+++ src/modules/calc.js
@@ -0,0 +1,41 @@
+const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block');
+    const calcType = calcBlock.querySelector('.calc-type');
+    const calcSquare = calcBlock.querySelector('.calc-square');
+    const calcCount = calcBlock.querySelector('.calc-count');
+    const calcDay = calcBlock.querySelector('.calc-day');
+    const total = document.getElementById('total');
+
+    const countCalc = () => {
+        const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+        const calcSquareValue = calcSquare.value;
+
+        let totalValue = 0;
+        let calcCountValue = 1;
+        let calcDayValue = 1;
+
+        if (calcCount.value > 1) {
+            calcCountValue += +calcCount.value / 10;
+        }
+
+        if (calcDay.value && calcDay.value < 5) {
+            calcDayValue = 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+            calcDayValue = 1.5;
+        }
+
+        if (calcType.value && calcSquare.value) {
+            totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+        } else {
+            totalValue = 0;
+        }
+
+        total.textContent = totalValue;
+    }
+
+    calcBlock.addEventListener('input', (e) => {
+        countCalc()
+    })
+}
+
+export default calc;
