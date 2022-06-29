d441a853d6534201b63d15971049bb62edb60f90
lavsora <ya.karpich@gmail.com>
2022-06-29 15:54:09 +0300

    slider create

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,7 +2,6 @@ const menu = () => {
     const menu = document.querySelector('menu');
 
     document.addEventListener('click', (e) => {
-        console.log(e.target.closest('.close-btn'))
         if (e.target.closest('.menu')) {
             menu.classList.add('active-menu')
         } else if (e.target.classList.contains('close-btn') || !e.target.closest('.active-menu')) {
